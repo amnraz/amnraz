fb190850a3c71d5bcb0f96eeb8d841ec513de0fc
Aman Kumar <129107515+amnraz@users.noreply.github.com>
2025-03-22 16:23:53 +0530

    task management

---
+++ task.js
@@ -0,0 +1,11 @@
+let tasks = [
+    "Complete all pending Assignment ",
+    "Read a chapter from a book",
+    "Go for a 30-minute walk",
+    "Revise all topic covers in this week ",
+    "Practice coding challenges"
+];
+tasks.shift()
+tasks.unshift("Plan the ohle day", "Typing practice 30min")
+tasks[tasks.length-1]= "Preperaring for Evoluation"
+console.log(tasks)
